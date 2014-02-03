dcbb7ac8213dffc9fb0a5b4ef4a793a50dd656fb
Derrick Gilland <dgilland@gmail.com>
2014-02-03 17:30:51 -0500

    Release v0.2.1.

--- alchy/__init__.py
+++ alchy/__init__.py
@@ -1,6 +1,10 @@
 
-__version__ = '0.2.0'
-
 from .model import ModelBase, make_declarative_base
 from .query import Query
 from .manager import Manager, ManagerBase
+
+__version__ = '0.2.1'
+__author__ = 'Derrick Gilland <dgilland@gmail.com>'
+__all__ = ['ModelBase', 'make_declarative_base', 'Query', 'Manager', 'ManagerBase']
+
+print locals()
