17c44dc7dd241b79f419c031b4629957cbb7d629
Derrick Gilland <dgilland@gmail.com>
2014-02-24 08:22:34 -0500

    Release v0.4.2.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name = 'alchy',
-    version = '0.4.1',
+    version = '0.4.2',
     url = 'https://github.com/dgilland/alchy',
     license = 'MIT',
     author = 'Derrick Gilland',
