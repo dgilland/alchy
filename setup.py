1f0ab15610725203247773479bf44ee4258dbb79
Derrick Gilland <dgilland@gmail.com>
2014-02-27 17:39:27 -0500

    Minor style changes.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """
 alchy
------
+=====
 
 The SQLAlchemy enhancement library.
 
@@ -10,21 +10,21 @@ Documentation: https://github.com/dgilland/alchy
 from setuptools import setup
 
 setup(
-    name = 'alchy',
-    version = '0.4.2',
-    url = 'https://github.com/dgilland/alchy',
-    license = 'MIT',
-    author = 'Derrick Gilland',
-    author_email = 'dgilland@gmail.com',
-    description = 'SQLAlchemy enhancement library',
-    long_description = __doc__,
-    packages = ['alchy'],
-    install_requires = [
+    name='alchy',
+    version='0.4.2',
+    url='https://github.com/dgilland/alchy',
+    license='MIT',
+    author='Derrick Gilland',
+    author_email='dgilland@gmail.com',
+    description='SQLAlchemy enhancement library.',
+    long_description=__doc__,
+    packages=['alchy'],
+    install_requires=[
         'SQLAlchemy>=0.9.0'
     ],
-    test_suite = 'tests',
-    keywords = 'sqlalchemy databases',
-    classifiers = [
+    test_suite='tests',
+    keywords='sqlalchemy databases',
+    classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
