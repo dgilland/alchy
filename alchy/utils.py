1a35d816e9fae189830bbae8ac93c6cf36eee75c
Derrick Gilland <dgilland@gmail.com>
2014-05-02 12:31:04 -0400

    Add flatten util methods.

--- alchy/utils.py
+++ alchy/utils.py
@@ -37,11 +37,30 @@ def has_primary_key(metadict):
                if isinstance(column, Column))
 
 
-REGEX_FIRST_CAP = re.compile('(.)([A-Z][a-z]+)')
-REGEX_ALL_CAP = re.compile('([a-z0-9])([A-Z])')
-
-
 def camelcase_to_underscore(string):
     """Convert string from CamelCase to under_score"""
+    REGEX_FIRST_CAP = re.compile('(.)([A-Z][a-z]+)')
+    REGEX_ALL_CAP = re.compile('([a-z0-9])([A-Z])')
+
     first_cap = REGEX_FIRST_CAP.sub(r'\1_\2', string)
     return REGEX_ALL_CAP.sub(r'\1_\2', first_cap).lower()
+
+
+def iterflatten(items):
+    """Return iterator which flattens list/tuple of lists/tuples
+    >>> to_flatten = [1, [2,3], [4, [5, [6]], 7], 8]
+    >>> assert list(iterflatten(to_flatten)) == [1,2,3,4,5,6,7,8]
+    """
+    for item in items:
+        if isinstance(item, (list, tuple)):
+            for itm in flatten(item):
+                yield itm
+        else:
+            yield item
+
+
+def flatten(items):
+    """Return flattened list of a list/tuple of lists/tuples
+    >>> assert flatten([1, [2,3], [4, [5, [6]], 7], 8]) == [1,2,3,4,5,6,7,8]
+    """
+    return list(iterflatten(items))
